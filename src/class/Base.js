4d2ea0997b94f9ef546b539f416c146ed03942a9
pinghuazhuang <zphua2016@gmail.com>
2019-12-31 14:40:47 +0800

    Update.

---
+++ src/class/Base.js
@@ -0,0 +1,7 @@
+/**
+ * 模仿 element-ui 中的 message
+ */
+
+export default class Base {
+    constructor () {}
+}
---
+++ src/class/ZTableRow.js
@@ -0,0 +1,5 @@
+export class ZTableRow {
+    static des = '行对象.'
+
+    constructor () {}
+}
