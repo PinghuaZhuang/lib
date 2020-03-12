591f26cd60cd1016718c208d4e6c92267e1c7bba
pinghuazhuang <zphua2016@gmail.com>
2020-03-12 14:03:54 +0800

    STYLE.

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -65,10 +65,11 @@ export function toTree ( arr, keys ) {
  * @param { Buffer } buffer
  */
 export function transformArrayBufferToBase64(buffer) {
-    var binary = '';
-    var bytes = new Uint8Array(buffer);
-    for (var len = bytes.byteLength, i = 0; i < len; i++) {
+    let binary = '', i = 0
+    let bytes = new Uint8Array(buffer)
+    let len = bytes.byteLength
+    for (; i < len; i++) {
       binary += String.fromCharCode(bytes[i]);
     }
     return window.btoa(binary);
-  }
+}
