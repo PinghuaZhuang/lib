368299521e77bb7309949447450041d2c08c91ee
PinghuaZhuang <zphua2016@gmail.com>
2022-03-21 11:58:07 +0800

    update: add hooks. update mixin.less

---
+++ src/vue/utils/hooks.js
@@ -0,0 +1,102 @@
+/* eslint-disable */
+import Vue from "vue";
+// import once from "lodash/once";
+
+const map = new WeakMap();
+const Deep = { target: null };
+
+// const createWatchs = (target, inputs, vm, update) => {
+//   const callbackId = `callback_${target.uid}`;
+//   let watchs = target.callbacks[callbackId];
+//   if (watchs == null) {
+//     watchs = target.callbacks[callbackId] = once(() =>
+//       inputs.forEach((ob) => {
+//         vm.$watch(() => ob.visible, update);
+//       })
+//     );
+//   }
+//   return watchs;
+// };
+
+// const createHook = (hookName, target, vm, callback) => {
+//   let emitFn = target.effects[`emit_${target.uid}`];
+//   const hookTag = `hook:${hookName}`;
+
+//   if (emitFn == null) {
+//     emitFn = target.effects[target.uid] = () => {
+//       vm.$off(hookTag, callback);
+//       vm.$on(hookTag, callback);
+//     };
+//   }
+//   emitFn();
+// };
+
+// 不同的返回true
+const diffDeps = (deps, target) => {
+  const depsId = `deps_${target.uid}`;
+  if (deps.length === 0) return false;
+
+  let preDepsStates = target.callbacks[depsId];
+  if (preDepsStates == null) {
+    target.callbacks[depsId] = deps;
+    return true;
+  }
+  const ret =
+    preDepsStates.length === deps.length &&
+    preDepsStates.every((o, i) => deps[i] === o);
+  target.callbacks[depsId] = deps;
+  return !ret;
+};
+
+export const useState = (defaultVisible) => {
+  const target = map.get(Deep.target);
+  let state = target.states[target.uid];
+  if (state == null) {
+    state = Vue.observable({
+      visible: !!defaultVisible,
+      setVisible(visible) {
+        state.visible = visible;
+      },
+    });
+    target.states[target.uid] = state;
+  }
+  target.uid++;
+  return [state.visible, state.setVisible];
+};
+
+export const useCallback = (callback, deps) => {
+  if (deps == null) return callback;
+
+  const target = map.get(Deep.target);
+  const { callbacks } = target;
+
+  // 依赖发生变化直接返回
+  if (deps && diffDeps(deps, target)) return callback;
+
+  let ret = callbacks[target.uid];
+  if (ret == null) {
+    ret = callback;
+    callbacks[target.uid] = ret;
+  }
+
+  target.uid++;
+  return ret;
+};
+
+const globalMixin = {
+  created() {
+    Deep.target = this;
+    map.set(this, {
+      states: {},
+      callbacks: {},
+      effects: {},
+      uid: 0,
+    });
+  },
+  beforeUpdate() {
+    Deep.target = this;
+    map.get(this).uid = 0;
+  },
+};
+
+Vue.mixin(globalMixin);
